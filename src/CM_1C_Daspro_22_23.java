3fc57a6d1aad886ebce9cf1a47866813f39adcbd
keizyaa <tsabitakeizya@gmail.com>
2024-12-10 11:58:37 +0700

    Progress 1

---
+++ src/CM_1C_Daspro_22_23.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class CM_1C_Daspro_22_23 {
+    
+    static String[] namaItem = new String[10];
+    static String[] kategoriItem = new String[10];
+    static int[] stokItem = new int[10];
+    static int jumlahItem = 0; 
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    static void menu() {
+        System.out.println("======= MENU INVENTORI KAFE =======");
+        System.out.println("1. Tampilkan Inventori");
+        System.out.println("2. Tambah Stok untuk Item yang Ada");
+        System.out.println("3. Tambah Item");
+        System.out.println("4. Exit");
+    }
+
+    static void tampilData() {
+        System.out.println("====== DATA INVENTORI ====== ");
+        
+    }
+}
