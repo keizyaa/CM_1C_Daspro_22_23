506ec0a5b164a4d7cf0a5772ab4c480776469dbd
keizyaa <tsabitakeizya@gmail.com>
2024-12-10 13:10:15 +0700

    Progress2

--- src/CM_1C_Daspro_22_23.java
+++ src/CM_1C_Daspro_22_23.java
@@ -5,14 +5,13 @@ public class CM_1C_Daspro_22_23 {
     static String[] namaItem = new String[10];
     static String[] kategoriItem = new String[10];
     static int[] stokItem = new int[10];
-    static int jumlahItem = 0; 
-    static Scanner scanner = new Scanner(System.in);
+    static int jmlItem = 0; 
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         
         
     }
-    
     static void menu() {
         System.out.println("======= MENU INVENTORI KAFE =======");
         System.out.println("1. Tampilkan Inventori");
@@ -20,10 +19,15 @@ public class CM_1C_Daspro_22_23 {
         System.out.println("3. Tambah Item");
         System.out.println("4. Exit");
     }
-
     static void tampilData() {
-        System.out.println("====== DATA INVENTORI ====== ");
-        System.out.println();
-        
+        System.out.println("======= DATA INVENTORI ======= ");
+        System.out.println("No\tNama Item\tKategori\nStok");
+        System.out.println("------------------------------");
+        for (int i = 0; i < jmlItem; i++) {
+            System.out.print((i+1) + "\t");
+            System.out.print(namaItem[i] + "\t\t");
+            System.out.print(kategoriItem[i] + "\t\t");
+            System.out.print(stokItem[i]);
+        }
     }
 }
